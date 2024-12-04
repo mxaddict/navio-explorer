bf7e55cf125215c187e02cd788f780a5609a86d8
mxaddict <mxaddict@kryptic.sh>
2024-12-04 08:34:45 +0800

    Pay will be in on monday. - Anon

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?
 $class_a="text-blue-700 dark:text-blue-600";
 $class_p="text-gray-900 dark:text-white";
+$action = isset($_GET['a']) ? $_GET['a'] : '';
 ?>
 <!doctype html>
 <html class="dark">
@@ -84,19 +85,19 @@ $class_p="text-gray-900 dark:text-white";
       </div>
       <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
         <li>
-          <a href="./" class="<?=(empty($_GET)?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+          <a href="./" class="<?=($action==""?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
         </li>
         <li>
-          <a href="blocks/" class="<?=($_GET["a"]=="blocks"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blocks</a>
+          <a href="blocks/" class="<?=($action=="blocks"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blocks</a>
         </li>
         <li>
-          <a href="peers/" class="<?=($_GET["a"]=="peers"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Peers</a>
+          <a href="peers/" class="<?=($action=="peers"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Peers</a>
         </li>
         <li>
-          <a href="rich-list/" class="<?=($_GET["a"]=="rich-list"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Rich List</a>
+          <a href="rich-list/" class="<?=($action=="rich-list"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Rich List</a>
         </li>
         <li>
-          <a href="stats/" class="<?=($_GET["a"]=="stats"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Stats</a>
+          <a href="stats/" class="<?=($action=="stats"?$class_a . " ":$class_p." ")?>block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Stats</a>
         </li>
         <li>
           <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Network <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
