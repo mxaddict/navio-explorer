3eacdf71b768cf458f3deb645cc41be13e303b9f
mxaddict <mxaddict@kryptic.sh>
2024-12-04 19:33:48 +0800

    In a perfect world, this would work. - Anon

--- main.php
+++ main.php
@@ -46,12 +46,12 @@
 		{
 			$latest_block=0;
 		}
-		$sql="SELECT 
+		$sql="SELECT
 		blocks.block_id,
 		blocks.hash,
 		blocks.data as blockData,
 		txs.data as txData
-		FROM blocks 
+		FROM blocks
 		LEFT JOIN txs ON txs.block_hash=blocks.hash AND txs.txno=1 AND blocks.network_id=txs.network_id
 		WHERE blocks.network_id=:network_id
 		ORDER BY blocks.id DESC LIMIT 10";
