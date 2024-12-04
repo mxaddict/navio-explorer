2bde27e9fbae16754ff25841189e1d9eb9b5e923
mxaddict <mxaddict@kryptic.sh>
2024-12-04 10:06:07 +0800

    I hate it when you guys don't talk. - Anon

---
+++ 404.php
@@ -0,0 +1,8 @@
+<? include "init.php"; ?> 
+<? include "header.php"; ?>
+<?= title("404: Not Found"); ?>
+<div class="p-4 w-full">
+    <h4 class="text-lg font-bold text-white dark:text-white">404: Not Found</h4>
+    <p class="text-white dark:text-white">You lost anon?</p>
+</div>
+<? include "footer.php"; ?>
