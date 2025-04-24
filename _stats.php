0ad2fab5e5266f92db8cfc7cd98b86be512e54bf
sakdeniz <38104512+sakdeniz@users.noreply.github.com>
2025-04-24 13:13:00 +0300

    Added block times chart

--- _stats.php
+++ _stats.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <div id="chart1" class="w-full" style="min-height: 365px;width:100%"></div>
 <div id="chart2" class="w-full" style="min-height: 365px;width:100%"></div>
+<div id="chart3" class="w-full" style="min-height: 365px;width:100%"></div>
 <?
 $sql="SELECT block_id,JSON_EXTRACT(blocks.data, '$.size') AS size,JSON_EXTRACT(blocks.data, '$.time') AS time,JSON_EXTRACT(blocks.data, '$.difficulty') AS difficulty FROM blocks WHERE network_id=:network_id ORDER BY id DESC LIMIT 1000";
 $q=$GLOBALS['dbh']->prepare($sql);
@@ -11,9 +12,10 @@ if ($q->rowCount()>0)
   while($row=$q->fetch(PDO::FETCH_ASSOC))
   {
     $arr_sizes[]=$row["size"];
-    $arr_times[]=$row["time"];
+    if ($previousBlockTime) $arr_times[]=($previousBlockTime-$row["time"]);
     $arr_difficulties[]=$row["difficulty"];
     $arr_block_ids[]=$row["block_id"];
+    $previousBlockTime=$row["time"];
   }
 }
 ?>
@@ -125,5 +127,60 @@ if ($q->rowCount()>0)
   }
 };
 var chart = new ApexCharts(document.querySelector("#chart2"), options);
+chart.render();
+</script>
+<script>
+  var options = {
+    series: [{
+      name: "Block Time",
+      data: [<?=implode(",",$arr_times)?>]
+    }],
+    chart: {
+      type: 'area',
+      height: 350,
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Block Times',
+      align: 'left',
+      style: {
+        fontSize:  '12pt',
+        fontWeight:  'bold',
+        color:  '#ffffff'
+      },
+    },
+    subtitle: {
+      text: 'Latest 1000 Block',
+      align: 'left',
+      style: {
+        fontSize:  '12pt',
+        fontWeight:  'bold',
+        color:  '#ffffff'
+      },          
+    },
+    labels:[<?=implode(",",$arr_block_ids)?>],
+    xaxis: {
+     labels: {
+      show: false,
+    }
+  },
+  yaxis: {
+    opposite: true
+  },
+  legend: {
+    horizontalAlign: 'left',
+    colors: 'white',
+    useSeriesColors: false
+  }
+};
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
 chart.render();
 </script>
